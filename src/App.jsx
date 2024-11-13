124f0c1d651e24a8732e1476b5e1c4eff66f09e0
Reeya Joseph <j.reeya@wustl.edu>
2024-11-13 14:11:37 -0600

    Added the status reports to determine who is next and also to keep a log of the history

--- src/App.jsx
+++ src/App.jsx
@@ -57,6 +57,8 @@ function Board() {
 }
 
 export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
   return (
     <div className="game">
       <div className="game-board">
