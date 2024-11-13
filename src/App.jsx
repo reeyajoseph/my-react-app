deea08794b13f4878a010b1473534c58cba41f1f
Reeya Joseph <j.reeya@wustl.edu>
2024-11-13 12:03:27 -0600

    Added the onSquareClick function into Square and the handleClick concept to the Board function, each of these are to allow them to communicate with each other and the handleClick function was updated to include the array index parameter and defined the setSquare to let it be known the status has changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import { useState } from 'react';
 
-function Square({value}) {
-  return <button className="square">{value}</button>
+function Square({value, onSquareClick}) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
+  
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} />
+        <Square value={squares[0]} onSquareClick={() =>handleClick(0)} />
         <Square value={squares[1]}/>
         <Square value={squares[2]}/>
       </div>
